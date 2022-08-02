f655a5bcc1ceb9a60a6528be7bb503839a9e0bb6
jeet136@yahoo.com <jeet136@yahoo.com>
2022-08-02 14:51:45 +0530

    remove the subnet

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   name = var.name
   vpc_cidr = var.vpc_cidr
 
-  availability_zone = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  availability_zone = ["us-east-2a", "us-east-2b"]
   private_subnets = ["10.0.1.0/24", "10.1.1.0/24"]
   public_subnets  = ["10.0.0.0/24", "10.1.0.0/24"]
 
